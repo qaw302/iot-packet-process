aad21a4556985b1779b2ea758f80c31d01359b5f
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-12 17:42:58 +0900

    backup

--- src/main/java/com/nhnacademy/node/MqttMessageProcessingNode.java
+++ src/main/java/com/nhnacademy/node/MqttMessageProcessingNode.java
@@ -1,12 +1,14 @@
 package com.nhnacademy.node;
 
+import com.nhnacademy.message.JsonMessage;
+import com.nhnacademy.message.Message;
+import com.nhnacademy.system.UndefinedJsonObject;
 import com.nhnacademy.wire.Wire;
 
 public class MqttMessageProcessingNode extends InputOutputNode {
 
     protected MqttMessageProcessingNode(String id) {
         super(id, 1);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
@@ -15,10 +17,20 @@ public class MqttMessageProcessingNode extends InputOutputNode {
 
     @Override
     void process() {
-        for(int i = 0;i<getInputWireCount();i++){
+        for (int i = 0; i < getInputWireCount(); i++) {
             Wire wire = getInputWire(i);
-            if(wire == null || !wire.hasMessage())
+            if (wire == null || !wire.hasMessage())
                 continue;
+            Message message = wire.get();
+            if (!(message instanceof JsonMessage))
+                continue;
+            JsonMessage jsonMessage = (JsonMessage) message;
+            // if (!(JsonMessage.getDestJsonObject(jsonMessage,
+            // new String[] { "payload", "deviceInfo", "tenantName" }) instanceof
+            // UndefinedJsonObject))
+            // System.out.println(jsonMessage.getJsonObject());
+            System.out.println(JsonMessage.getDestJsonObject(jsonMessage,
+                    new String[] { "payload", "deviceInfo", "tenantName" }));
         }
     }
 
