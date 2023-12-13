5c41c6912cb513473bca1b798e06efbaa941bd0b
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-13 17:39:38 +0900

    끝

--- src/main/java/com/nhnacademy/node/MqttMessageGenerator.java
+++ src/main/java/com/nhnacademy/node/MqttMessageGenerator.java
@@ -26,7 +26,16 @@ public class MqttMessageGenerator extends InputOutputNode {
             if (!(message instanceof JsonMessage))
                 continue;
             JSONObject jsonObject = ((JsonMessage) message).getJsonObject();
-            String topic = "data/s/"+
+            JSONObject payload = (JSONObject) jsonObject.get("payload");
+            String topic = "data/s/" + payload.get("site") + "/b/" + payload.get("branch") + "/p/"
+                    + payload.get("place") + "/d/" + payload.get("devEui") + "/e/" + payload.get("sensor");
+            payload = new JSONObject();
+            payload.put("value", jsonObject.get("value"));
+            payload.put("time", System.currentTimeMillis());
+            jsonObject = new JSONObject();
+            jsonObject.put("topic", topic);
+            jsonObject.put("payload", payload);
+            output(0, new JsonMessage(jsonObject));
         }
     }
 
