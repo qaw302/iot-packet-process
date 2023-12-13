2b821fe9a61c7d53f6a31783795418d7c49a5dcb
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-13 17:41:09 +0900

    .

---
+++ src/main/java/com/nhnacademy/node/ModbusMapperKeywordToRegister.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.node;
+
+public class ModbusMapperKeywordToRegister extends InputOutputNode {
+
+    protected ModbusMapperKeywordToRegister(String id) {
+        super(id, 1);
+    }
+
+    @Override
+    void preprocess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'preprocess'");
+    }
+
+    @Override
+    void process() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+
+    @Override
+    void postprocess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'postprocess'");
+    }
+
+}
