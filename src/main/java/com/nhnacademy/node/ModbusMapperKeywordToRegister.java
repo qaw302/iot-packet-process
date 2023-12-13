8e5c656c5119dac8b42a22af5570361f23c9bec2
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-13 17:44:19 +0900

    backup

--- src/main/java/com/nhnacademy/node/ModbusMapperKeywordToRegister.java
+++ src/main/java/com/nhnacademy/node/ModbusMapperKeywordToRegister.java
@@ -1,27 +1,54 @@
 package com.nhnacademy.node;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.nhnacademy.message.JsonMessage;
+import com.nhnacademy.message.Message;
+import com.nhnacademy.wire.Wire;
+
 public class ModbusMapperKeywordToRegister extends InputOutputNode {
+    public JSONObject registerAddressMappingTable;
 
     protected ModbusMapperKeywordToRegister(String id) {
         super(id, 1);
+        JSONParser jsonParser = new JSONParser();
+        try {
+            Reader reader = new FileReader("src/main/resources/registerAddressMappingTable.json");
+            registerAddressMappingTable = (JSONObject) jsonParser.parse(reader);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     void preprocess() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'preprocess'");
     }
 
     @Override
     void process() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+        for (int i = 0; i < getInputWireCount(); i++) {
+            Wire wire = getInputWire(i);
+            if (wire == null || !wire.hasMessage())
+                continue;
+            Message message = wire.get();
+            if (!(message instanceof JsonMessage))
+                continue;
+            JSONObject jsonObject = ((JsonMessage) message).getJsonObject();
+        }
     }
 
     @Override
     void postprocess() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'postprocess'");
     }
 
 }
