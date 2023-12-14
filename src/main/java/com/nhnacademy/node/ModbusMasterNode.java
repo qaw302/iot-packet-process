ce9bf26caf6cb99e7bd5aba168a89b49e68010ce
{qaw302} <{osshun21@gmail.com}>
2023-12-14 15:14:03 +0900

    feat : ModbusClient,Master 구현완료
    ModbusClient - server랑 socket연결로 데이터 요청,
    ModbusMaster - ModbusClient에서 받은 데이터 다음 노드로 전달
    ModbusRequestMaker - modbusrequest 생성해주는 클래스

--- src/main/java/com/nhnacademy/node/ModbusMasterNode.java
+++ src/main/java/com/nhnacademy/node/ModbusMasterNode.java
@@ -1,33 +1,21 @@
 package com.nhnacademy.node;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.HashMap;
 import java.util.Queue;
-import java.util.Set;
+import org.json.simple.JSONObject;
 
-import org.json.JSONObject;
-
-import com.nhnacademy.message.BufferMessage;
+import com.nhnacademy.message.JsonMessage;
 import com.nhnacademy.system.ModbusClient;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ModbusMasterNode extends InputNode {
+    private static final int ADDRESS = 11101;
     private ModbusClient modbusClient;
     private Queue<JSONObject> dataQueue;
 
-    protected ModbusMasterNode(String id) {
+    public ModbusMasterNode(String id) {
         super(id, 1);
-    }
-
-    private static final HashMap<Integer, String> functionCodeMap = new HashMap<>();
-    static {
-        functionCodeMap.put(3, "makeReadHoldingRegistersRequest");
-        functionCodeMap.put(4, "makeReadInputRegistersRequest");
-        functionCodeMap.put(6, "makeWriteSingleRegisterRequest");
-        functionCodeMap.put(16, "makeWriteMultipleRegistersRequest");
     }
 
     @Override
@@ -39,16 +27,20 @@ public class ModbusMasterNode extends InputNode {
 
     @Override
     synchronized void process() {
+        modbusClient.setAddress(ADDRESS); // gyeongnam/class_a/24e124128c067999/temperature
         while (!thread.isInterrupted()) {
-            if (dataQueue.isEmpty()) {
+            if (!dataQueue.isEmpty()) {
+                JsonMessage message = new JsonMessage(dataQueue.poll());
+                log.info("output message : {}", message.toString());
+                output(message, 0);
+
+            } else {
                 try {
                     wait();
                     log.info("Queue is empty. Thread is watting");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } else {
-
             }
         }
 
@@ -57,134 +49,6 @@ public class ModbusMasterNode extends InputNode {
     @Override
     void postprocess() {
         modbusClient.interrupt();
-    }
-
-    public static Set<Integer> getFunctionKeys() {
-        return functionCodeMap.keySet();
-    }
-
-    public static String getMethodName(int functionCode) {
-        return functionCodeMap.get(functionCode);
-    }
-
-    public static byte[] addMBAPHeader(int transactionId, int unitId, byte[] pdu) {
-        byte[] adu = new byte[7 + pdu.length];
-        ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
-
-        buffer.putInt(transactionId);
-        adu[0] = buffer.get(2);
-        adu[1] = buffer.get(3);
-
-        adu[2] = 0;
-        adu[3] = 0;
-
-        buffer.clear();
-        buffer.putInt(pdu.length);
-        adu[4] = buffer.get(2);
-        adu[5] = buffer.get(3);
-
-        buffer.clear();
-        buffer.putInt(unitId);
-        adu[6] = buffer.get(3);
-        System.arraycopy(pdu, 0, adu, 7, pdu.length);
-
-        return adu;
-    }
-
-    public static byte[] makeReadHoldingRegistersRequest(int address, int quantity) {
-        byte[] frame = new byte[5];
-        ByteBuffer b = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
-
-        // PDU의 function code
-        frame[0] = 0x03;
-
-        // PDU의 data
-        b.putInt(address);
-        frame[1] = b.get(2);
-        frame[2] = b.get(3);
-
-        b.clear();
-        b.putInt(quantity);
-        frame[3] = b.get(2);
-        frame[4] = b.get(3);
-
-        return frame;
-    }
-
-    /**
-     * 0x04 request
-     * 
-     * @param address
-     * @param quantity
-     * @return
-     */
-    public static byte[] makeReadInputRegistersRequest(int address, int quantity) {
-        byte[] frame = new byte[5];
-
-        ByteBuffer b = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
-        b.putInt(address);
-
-        // PDU의 function code
-        frame[0] = 0x04;
-
-        // PDU의 data 부분
-        frame[1] = b.get(2);
-        frame[2] = b.get(3);
-
-        b.clear();
-        b.putInt(quantity);
-        frame[3] = b.get(2);
-        frame[4] = b.get(3);
-
-        return frame;
-    }
-
-    public static byte[] makeWriteSingleRegisterRequest(int address, int value) {
-        byte[] frame = new byte[5];
-        ByteBuffer b = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN); // Big_endian 처리
-
-        frame[0] = 0x06; // 헥사코드, PDU function code
-
-        b.putInt(address); // PDU data code
-        frame[1] = b.get(2);
-        frame[2] = b.get(3);
-
-        b.clear(); // PDU quantity data
-        b.putInt(value);
-        frame[3] = b.get(2);
-        frame[4] = b.get(3);
-
-        return frame;
-    }
-
-    public byte[] makeWriteMultipleRegistersRequest(int address, int quantity, byte[] pdu) {
-        // fc, address, address, quantity, quantity, byte count, Register values(Nbytes)
-        // address : 10101 (0010 0111 0111 0101)-> {16, 39, 117, x, x, x, x ...}
-
-        byte[] frame = new byte[6 + pdu.length];
-
-        ByteBuffer b = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
-
-        frame[0] = 0x10;
-
-        b.putInt(address);
-        frame[1] = b.get(2);
-        frame[2] = b.get(3);
-
-        b.clear();
-        b.putInt(quantity);
-        frame[3] = b.get(2);
-        frame[4] = b.get(3);
-
-        frame[5] = (byte) (quantity < 127 || quantity > 0 ? quantity * 2 : quantity);
-
-        if (quantity * 2 != pdu.length) {
-            throw new IllegalArgumentException();
-        }
-
-        System.arraycopy(pdu, 0, frame, 6, pdu.length);
-
-        return frame;
     }
 
 }
--- src/main/java/com/nhnacademy/system/ModbusClient.java
+++ src/main/java/com/nhnacademy/system/ModbusClient.java
@@ -4,23 +4,30 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 /* 
  * Modbus server로부터 센서값을 받아오기위해 Soket 통신을 하는 역할
+ * 서버 당 MasterNode 하나씩 생성
+ * 필드
+ * - serverList
+ * - dataQueue : server에서 받은 response를 저장해 두는 큐
 */
 @Slf4j
 public class ModbusClient extends Thread {
     private static Queue<JSONObject> dataQueue = new LinkedList<>();
-    static final String HOST = "localhost";
-    static final int PORT = 11502;
+    static final String HOST = "172.19.0.6";
+    static final int PORT = 502;
     private final int unitId;
     static int transactionId = 0;
+    private int address = -1;
 
     private Socket socket;
     private BufferedInputStream inputStream;
@@ -38,44 +45,67 @@ public class ModbusClient extends Thread {
             log.info("[Client] - connect server");
             inputStream = new BufferedInputStream(socket.getInputStream());
             outputStream = new BufferedOutputStream(socket.getOutputStream());
+
+            sendAndReceive();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public Queue<JSONObject> getQueue() {
         return dataQueue;
     }
 
-    private void requestToServer() {
+    private synchronized void sendAndReceive() {
+        Thread requestThread = new Thread() {
+            @Override
+            public void run() {
+                log.info("request Thread is running");
+                setName("request");
+                while (!this.isInterrupted()) {
+                    try {
 
-    }
+                        while (address == -1) {
+                            wait();
+                        }
 
-    private void receiveResponse() {
-        byte[] receivedData = new byte[1024];
-        int receiveLength;
-        JSONObject data = new JSONObject();
-        while (socket.isConnected()) {
-            try {
-                receiveLength = inputStream.read(new byte[1024], 0, 1024);
-                byte[] pdu = new byte[receiveLength - 7];
-                System.arraycopy(receivedData, 7, pdu, 0, receiveLength);
-                data.append("transactionId", ++transactionId);
-                data.append("unitId", unitId);
-                data.append("pdu", pdu);
+                        // request 생성 후 server에 요청 보내기
+                        byte[] request = ModbusRequsetMaker.addMBAPHeader(++transactionId, unitId,
+                                ModbusRequsetMaker.makeReadHoldingRegistersRequest(address, 1));
+                        log.info("request : {}", Arrays.toString(request));
+                        outputStream.write(request);
+                        outputStream.flush();
+                        log.info("request sent");
 
-                dataQueue.add(data);
-                log.info("[Client] - data : {}", data.toString());
-                notifyAll();
+                        // 요청에대한 응답 받기
+                        byte[] receivedData = new byte[16];
+                        int receiveLength = inputStream.read(receivedData, 0, 16);
+                        log.info("received response");
+                        JSONObject data = new JSONObject();
+                        data.put("registerAddress", address);
+                        data.put("value", ((receivedData[receiveLength - 2] & 0xff) << 8)
+                                | (receivedData[receiveLength - 1] & 0xff));
+                        log.info("address : {}, value : {}", address, ((receivedData[receiveLength - 2] & 0xff) << 8)
+                                | (receivedData[receiveLength - 1] & 0xff));
+                        dataQueue.add(data);
+                        log.info("[Client] - data : {}", data.toString());
+                        Thread.sleep(1000);
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        };
+        requestThread.start();
     }
 
-    private void sendData() {
+    public synchronized void setAddress(int address) {
+        this.address = address;
+        notifyAll();
+    }
 
+    public int getAddres() {
+        return address;
     }
 }
