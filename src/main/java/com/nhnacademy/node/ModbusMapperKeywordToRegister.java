f6a294fa231dc415e1f660dadddf11ca311a2a54
doramonz <32805709+doramonz@users.noreply.github.com>
2023-12-14 14:14:27 +0900

    done

--- src/main/java/com/nhnacademy/node/ModbusMapperKeywordToRegister.java
+++ src/main/java/com/nhnacademy/node/ModbusMapperKeywordToRegister.java
@@ -44,6 +44,27 @@ public class ModbusMapperKeywordToRegister extends InputOutputNode {
             if (!(message instanceof JsonMessage))
                 continue;
             JSONObject jsonObject = ((JsonMessage) message).getJsonObject();
+            String branch = JsonMessage.getDestJsonObject(jsonObject, new String[] { "payload", "branch" })
+                    .get("branch").toString();
+            String place = JsonMessage.getDestJsonObject(jsonObject, new String[] { "payload", "place" }).get("place")
+                    .toString();
+            String devEui = JsonMessage.getDestJsonObject(jsonObject, new String[] { "payload", "devEui" })
+                    .get("devEui").toString();
+            String sensor = JsonMessage.getDestJsonObject(jsonObject, new String[] { "payload", "sensor" })
+                    .get("sensor")
+                    .toString();
+            long address = (long) ((JSONObject) JsonMessage.getDestJsonObject(registerAddressMappingTable,
+                    new String[] { "branch", branch, place, devEui }))
+                    .get(devEui);
+            address += (long) (JsonMessage.getDestJsonObject(registerAddressMappingTable,
+                    new String[] { "sensors", sensor })).get(sensor);
+            JSONObject result = new JSONObject();
+            JSONObject payload = new JSONObject();
+            payload.put("registerAddress", address);
+            payload.put("value", JsonMessage.getDestJsonObject(jsonObject, new String[] { "payload", "value" })
+                    .get("value"));
+            result.put("payload", payload);
+            output(0, new JsonMessage(result));
         }
     }
 
